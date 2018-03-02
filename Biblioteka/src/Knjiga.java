35b9d1f47db7d3b9ed9d0a7710ca9ec02b4c1dd4
Prikica <dejannn06@gmail.com>
2018-03-02 11:15:44 +0100

    Refaktorisana klasa Knjiga, uvedena klasa Autor

--- Biblioteka/src/Knjiga.java
+++ Biblioteka/src/Knjiga.java
@@ -1,10 +1,19 @@
 
 public class Knjiga {
-	private String naslov, isbn, izdavac, autori;
+	private String naslov, isbn, izdavac;
 	private int izdanje;
+	private Autor[] autori;
 
 	public String getNaslov() {
 		return naslov;
+	}
+
+	public Autor[] getAutori() {
+		return autori;
+	}
+
+	public void setAutori(Autor[] autori) {
+		this.autori = autori;
 	}
 
 	public void setNaslov(String naslov) {
@@ -25,14 +34,6 @@ public class Knjiga {
 
 	public void setIzdavac(String izdavac) {
 		this.izdavac = izdavac;
-	}
-
-	public String getAutori() {
-		return autori;
-	}
-
-	public void setAutori(String autori) {
-		this.autori = autori;
 	}
 
 	public int getIzdanje() {
